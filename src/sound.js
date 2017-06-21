140979daacb48fecdd11250cfd283a646695abe7
Lisa Uijtewaal <uijtewaallisa@gmail.com>
2017-06-21 08:48:01 +0200

    Commit

--- src/sound.js
+++ src/sound.js
@@ -6,7 +6,10 @@ const fruits = Object.keys(FRUIT);
 const ion = window.ion;
 
 ion.sound({
-    sounds: ['correct', 'wrong', 'wacht', 'gelijk', 'gewonnen', 'verloren', ...fruits].map(name => ({name})),
+    sounds: [
+        ...['correct', 'wrong'].map(name => ({ name})),
+        ...['wacht', 'gelijk', 'gewonnen', 'verloren', ...fruits].map(name => ({ name, supported: ['ogg']})),
+    ],
     volume: 1,
     path: './sounds/',
     preload: true
